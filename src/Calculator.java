51c577c7c162d3660a94c1ea93c4ad951ad3a3be
Gia Khang Cao <khang2004erwin@gmail.com>
2024-11-03 12:39:31 -0500

    Add Delete and Clear Buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -19,6 +20,47 @@ public class Calculator implements ActionListener {
         textField = new JTextField();
         textField.setBounds(50, 0, 300, 100);
         textField.setEditable(false);
+
+        plus = new JButton("+");
+        minus = new JButton("-");
+        multi = new JButton("*");
+        divide = new JButton("/");
+        equals = new JButton("=");
+        clear = new JButton("Clear");
+        decimal = new JButton(".");
+        percent = new JButton("%");
+        delete = new JButton("Delete");
+        operatorButtons[0] = plus;
+        operatorButtons[1] = minus;
+        operatorButtons[2] = multi;
+        operatorButtons[3] = divide;
+        operatorButtons[4] = equals;
+        operatorButtons[5] = clear;
+        operatorButtons[6] = decimal;
+        operatorButtons[7] = percent;
+        operatorButtons[8] = delete;
+
+        for (int i = 0; i < operatorButtons.length; i++) {
+            operatorButtons[i].addActionListener(this);
+            operatorButtons[i].setFocusable(false);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFocusable(false);
+        }
+
+        delete.setBounds(50, 300, 145, 50);
+        clear.setBounds(205, 300, 145, 50);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(Color.BLACK);
+
+        frame.add(clear);
+        frame.add(delete);
         frame.add(textField);
         frame.setVisible(true);
     }
